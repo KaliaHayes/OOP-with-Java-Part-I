033702ed10adab37c1bcfa179a30032c1e451b17
Kalia Hayes <kaliahayes@yahoo.com>
2017-09-11 23:14:03 -0500

    did the
    do

--- week3-045.Greatest/src/Greatest.java
+++ week3-045.Greatest/src/Greatest.java
@@ -2,8 +2,15 @@
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
-
-        return -1;
+        if(number1>=number2 && number1 >= number3){ //if num1 is greater, return the value of 1
+            return number1;
+        }
+        else if (number2 >= number1 && number2 >= number3) { //else return value of num2 because it's obvs greater
+            return number2;
+        }
+        else {
+            return number3;
+        }
     }
 
     public static void main(String[] args) {
